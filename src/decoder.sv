17a4ade3cf8700949e821f9741e9eaedfba99573
Abhiram Dasika <alfadelta10010@gmail.com>
2024-09-05 00:34:22 +0530

    spaces instead of tabs

--- src/decoder.sv
+++ src/decoder.sv
@@ -1,30 +1,30 @@
 module decoder(instIn, opcode, rs1, rs2, instOut, fn);
-	input logic [7:0] instIn;
-	output logic [1:0] rs1;
-	output logic [1:0] rs2;
-	output logic [7:0] instOut;
+  input logic [7:0] instIn;
+  output logic [1:0] rs1;
+  output logic [1:0] rs2;
+  output logic [7:0] instOut;
   output logic [1:0] opcode;
   output logic [1:0] fn;
-	always_comb
-		begin
+  always_comb
+    begin
       opcode = instIn[1:0];
-			instOut = instIn;
-			case(opcode)
+      instOut = instIn;
+      case(opcode)
 				2'b00: //Logic AND, OR, SRL, SLL
-					begin
+          begin
             rs1 = {instIn[4], instIn[6]};
 						rs2 = {instIn[5], instIn[7]};
             fn = {instIn[3], instIn[2]};
-					end
-				2'b01: //Branches BLT and BEQ
-					begin
+          end
+        2'b01: //Branches BLT and BEQ
+          begin
             rs1 = {1'b0, instIn[6]};
             rs2 = {1'b0, instIn[7]};
             fn = {1'bX, instIn[2]};
             //immediate = instIn[5:3];
-					end
+          end
 				2'b10: //Load-Store-PCE??????
-					begin
+           begin
             fn = {1'bX, instIn[2]};
             if (fn == 2'bX0)
               begin
@@ -44,9 +44,9 @@ module decoder(instIn, opcode, rs1, rs2, instOut, fn);
                 rs2 = 2'bX;
                 //immediate = instIn[7:4];
               end
-					end
+          end
 				2'b11: //Arithmetic-Jump-Halt
-					begin
+          begin
             fn = instIn[3:2];
             if (fn == 2'bX0)
               begin
@@ -59,13 +59,13 @@ module decoder(instIn, opcode, rs1, rs2, instOut, fn);
                 rs2 = 2'bX;
                 //immediate = instIn[7:4];
               end
-					end
-				default:
-					begin
+          end
+        default:
+          begin
 						rs1 = 2'bX;
 						rs2 = 2'bX;
 						fn = 2'bX;
-					end
-			endcase
-		end
+            end
+      endcase
+    end
 endmodule
