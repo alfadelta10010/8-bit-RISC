84a69082084b0cfe6ecf350314a89c4916728d98
Devesh B <65714343+devesh-b@users.noreply.github.com>
2024-09-06 00:50:00 +0530

    Update decoder.v
    to include four bit fn

--- src/decoder.v
+++ src/decoder.v
@@ -3,9 +3,9 @@ module decoder(instIn, rs1, rs2, fn, imm);
   output reg [1:0] rs1;
   output reg [1:0] rs2;
   reg [2:0] opcode;
-  output reg [0:0] fn;
+  output reg [3:0] fn;
   output reg [3:0] imm;
-  always_comb
+  always@ (*)
     begin
       opcode = instIn[2:0];
       case(opcode)
@@ -13,55 +13,55 @@ module decoder(instIn, rs1, rs2, fn, imm);
           begin
             rs1 = {instIn[4], instIn[6]};
             rs2 = {instIn[5], instIn[7]};
-            fn = instIn[3];
+            fn  = {instIn[3:0]};
             imm = 4'bX;
           end
         3'b001: // Branches BLT
           begin
             rs1 = {1'b0, instIn[6]};
             rs2 = {1'b0, instIn[7]};
-            fn = 1'bX;
+            fn = {1'bx, instIn[2:0]};
             imm = instIn[5:3];
           end
         3'b101: // Branches BEQ
           begin
             rs1 = {1'b0, instIn[6]};
             rs2 = {1'b0, instIn[7]};
-            fn = 1'bX;
+            fn = {1'bx, instIn[2:0]};
             imm = instIn[5:3];
           end
         3'b010: // Load
           begin
-            fn = 1'bX;
+            fn = {1'bx, instIn[2:0]};
             rs1 = {1'b0, instIn[7]};
             rs2 = 2'bX;
             imm = instIn[6:3];
           end
-        3'b010: // Store
+        3'b110: // Store
           begin
-            fn = 1'bX;
+            fn  = {1'bx, instIn[2:0]};
             rs1 = {1'b0, instIn[6]};
             rs2 = {1'b0, instIn[7]};
             imm = instIn[5:3];
           end
         3'b011: // Arithmetic ADD SUB
           begin
-            fn = instIn[3];
+            fn  = {instIn[3:0]};
             rs1 = {instIn[4], instIn[6]};
             rs2 = {instIn[5], instIn[7]};
             imm = 4'bX;
           end
         3'b100: // Logic SRL SLL
           begin
-            fn = instIn[3];
+            fn  = {instIn[3:0]};
             rs1 = {instIn[4], instIn[6]};
             rs2 = {instIn[5], instIn[7]};
             imm = 4'bX;
           end
         3'b111: // Jump + New Inst
           begin
-            fn = instIn[3];
-            if(fn == 1'b1) 
+            fn = instIn[3:0];
+            if(instIn[3] == 1'b1) 
               begin
                 rs1 = 2'bX;
                 rs2 = 2'bX;
