b1526eef847a0317ae004ab79ea383b5ff255b27
Om210 <142506156+Om210@users.noreply.github.com>
2024-09-05 12:21:52 +0530

    Update alu.sv
    Used adder_8 this time :)

--- src/alu.sv
+++ src/alu.sv
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// CompanOutReg: 
-// Engineer: 
-// 
-// Create Date: 05.09.2024 10:02:49
-// Design Name: 
-// Module Name: alu
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module alu(
            input [7:0] InReg1, InReg2,
@@ -26,10 +6,16 @@ module alu(
            input Flag,
            output [7:0] OutReg
            );
-
+           
+logic Carry;
 logic [7:0] Inv;
 assign Inv = ~InReg2 + 8'b00000001;
-
+adder_8 add_sub_instance (
+    .OutReg(OutReg),
+    .Carry(Carry),
+    .A(A),
+    .B(B)
+);
 always @(*) 
 begin
     if (CtrlSig == 3'b000 )
@@ -38,15 +24,25 @@ begin
         else 
             OutReg = ~(InReg1 | InReg2);  // NOR
     else if (CtrlSig == 3'b011)
+    begin
         if (Flag == 1)
-            OutReg = InReg1 + InReg2;     // Add 
-        else
-            OutReg = InReg1 + Inv;        // Sub
+        begin
+            A = InReg1;
+            B = InReg2;     // Add 
+        end 
+        else 
+        begin
+            A = InReg1;
+            B = Inv;  
+        end      // Sub
+    end
     else if (CtrlSig == 3'b100)
         if (Flag == 1)
             OutReg = InReg1 >> InReg2;    // SRL
         else
             OutReg = InReg1 << InReg2;    //SLL
+    else 
+        OutReg = 8'b00000000;
 end
 
 endmodule
