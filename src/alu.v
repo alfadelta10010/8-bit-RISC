9526f7bad73e2d995e87bea0cc3eb1285c5a6d9f
Devesh B <65714343+devesh-b@users.noreply.github.com>
2024-09-06 01:47:23 +0530

    Update alu.v

--- src/alu.v
+++ src/alu.v
@@ -21,47 +21,52 @@ module alu(
     .Cout(add_overflow)
   );
 
-   always @(*) begin
+  always @(*) begin
+    // Default assignments to avoid latches
+    out = 8'b0;
+    overflow = 1'b0;
+    a_temp = 8'b0;
+    b_temp = 8'b0;
+    cin = 1'b0;
+
     case(ctrl)
-      3'b000: 
-      begin
+      3'b000: begin
         if (Flag == 1)
-            out = ~(rs1 & rs2);  // NAND
+          out = ~(rs1 & rs2);  // NAND
         else 
-            out = ~(rs1 | rs2);  // NOR
-           end
-      3'b011: 
-        if (Flag == 1)
-        begin 
+          out = ~(rs1 | rs2);  // NOR
+      end
+      
+      3'b011: begin
+        if (Flag == 1) begin 
           a_temp = rs1;
           b_temp = ~rs2;
           cin = 1'b1; 
           out = add_out;
           overflow = add_overflow;
-        end
-        else begin
+        end else begin
           a_temp = rs1;
           b_temp = rs2;
           cin = 1'b0; 
           out = add_out;
           overflow = add_overflow;
         end
-      3'b100: 
-        if (Flag == 1) 
-        begin
-          out = rs1 << rs2[2:0]; 
-          overflow = 1'b0; 
-        end
-        else 
-        begin
-          out = rs1 >> rs2[2:0];
+      end
+      
+      3'b100: begin
+        if (Flag == 1) begin
+          out = rs1 << rs2[2:0];  // Shift left
+          overflow = 1'b0;
+        end else begin
+          out = rs1 >> rs2[2:0];  // Shift right
           overflow = 1'b0;
         end
-        default: 
-        begin
-          out = 8'bX;
-          overflow = 1'bX;
-        end
-      endcase
-   end
+      end
+
+      default: begin
+        out = 8'b0;    // Assign a valid default value to avoid X (undefined state)
+        overflow = 1'b0;
+      end
+    endcase
+  end
 endmodule
