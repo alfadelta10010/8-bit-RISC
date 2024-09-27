6a4125c73f68fcb7a4a42f94ce572e649edd2883
Om <cuber2116@gmail.com>
2024-09-27 23:42:13 +0530

    changed fn_tb to hold 4-bits

--- tb/decoder_tb.v
+++ tb/decoder_tb.v
@@ -3,7 +3,7 @@ module tb_decoder;
   reg [7:0] instIn_tb;
   wire [1:0] rs1_tb;
   wire [1:0] rs2_tb;
-  wire fn_tb;
+  wire [3:0] fn_tb;
   wire [3:0] imm_tb;
 
   decoder dut(
@@ -17,6 +17,7 @@ module tb_decoder;
   initial begin
     $dumpfile("dump.vcd");
     $dumpvars(0, tb_decoder);
+    $monitor("Time: %0t, instIn: %b, rs1: %b, rs2: %b, fn: %b, imm: %b", $time, instIn_tb, rs1_tb, rs2_tb, fn_tb, imm_tb);
     instIn_tb = 8'b10101000; // NOR
     #5 instIn_tb = 8'b10100000; // NAND
     #5 instIn_tb = 8'b10101001; // BLT
@@ -35,6 +36,6 @@ module tb_decoder;
     #5 instIn_tb = 8'b10100111; // Jump
     #10 $finish;
   end
-
+ 
 endmodule
 
