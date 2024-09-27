aff28217f69c1bbd8b428b874fa50e659c1f29ac
Om <cuber2116@gmail.com>
2024-09-27 23:09:50 +0530

    changed decoder TB to Verilog

--- tb/decoder_tb.sv
+++ tb/decoder_tb.sv
@@ -1,11 +1,11 @@
-// https://edaplayground.com/x/VgZc
-module tb_decoder(rs1_tb, rs2_tb, fn_tb, imm_tb);
-  logic [7:0] instIn_tb;
-  input logic [1:0] rs1_tb;
-  input logic [1:0] rs2_tb;
-  input logic [0:0] fn_tb;
-  input logic [3:0] imm_tb;
-  
+module tb_decoder;
+
+  reg [7:0] instIn_tb;
+  wire [1:0] rs1_tb;
+  wire [1:0] rs2_tb;
+  wire fn_tb;
+  wire [3:0] imm_tb;
+
   decoder dut(
     .instIn(instIn_tb), 
     .rs1(rs1_tb), 
@@ -13,27 +13,28 @@ module tb_decoder(rs1_tb, rs2_tb, fn_tb, imm_tb);
     .fn(fn_tb), 
     .imm(imm_tb)
   );
-  
-  initial
-    begin
-      $dumpfile("dump.vcd");
-      $dumpvars(0, tb_decoder);
-      instIn_tb <= 8'b10101000; // NOR
-      #5 instIn_tb <= 8'b10100000; // NAND
-      #5 instIn_tb <= 8'b10101001; // BLT
-      #5 instIn_tb <= 8'b10100001; // BLT
-      #5 instIn_tb <= 8'b10101010; // Load
-      #5 instIn_tb <= 8'b10100010; // Load
-      #5 instIn_tb <= 8'b10101011; // SUB
-      #5 instIn_tb <= 8'b10100011; // ADD
-      #5 instIn_tb <= 8'b10101100; // SLL
-      #5 instIn_tb <= 8'b10100100; // SRL
-      #5 instIn_tb <= 8'b10101101; // BEQ
-      #5 instIn_tb <= 8'b10100101; // BEQ
-      #5 instIn_tb <= 8'b10101110; // Store
-      #5 instIn_tb <= 8'b10100110; // Store
-      #5 instIn_tb <= 8'b10101111; // Undefined
-      #5 instIn_tb <= 8'b10100111; // Jump
-      #10 $finish;
-    end
+
+  initial begin
+    $dumpfile("dump.vcd");
+    $dumpvars(0, tb_decoder);
+    instIn_tb = 8'b10101000; // NOR
+    #5 instIn_tb = 8'b10100000; // NAND
+    #5 instIn_tb = 8'b10101001; // BLT
+    #5 instIn_tb = 8'b10100001; // BLT
+    #5 instIn_tb = 8'b10101010; // Load
+    #5 instIn_tb = 8'b10100010; // Load
+    #5 instIn_tb = 8'b10101011; // SUB
+    #5 instIn_tb = 8'b10100011; // ADD
+    #5 instIn_tb = 8'b10101100; // SLL
+    #5 instIn_tb = 8'b10100100; // SRL
+    #5 instIn_tb = 8'b10101101; // BEQ
+    #5 instIn_tb = 8'b10100101; // BEQ
+    #5 instIn_tb = 8'b10101110; // Store
+    #5 instIn_tb = 8'b10100110; // Store
+    #5 instIn_tb = 8'b10101111; // Undefined
+    #5 instIn_tb = 8'b10100111; // Jump
+    #10 $finish;
+  end
+
 endmodule
+
